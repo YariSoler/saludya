28f5b3711a8d2325d719cd3c582f0d6233828eb2
YariSoler <yithosoler15@gmail.com>
2026-04-30 22:04:50 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Auth
+
 import Login from "./pages/Login";
 import Recover from "./pages/Recover";
 
-// Registro
+
 import RegisterPaciente from "./pages/RegisterPaciente";
 
-// Dashboards
+
 import DashboardAdmin from "./pages/DashboardAdmin";
 import DashboardMedico from "./pages/DashboardMedico";
 import DashboardPaciente from "./pages/DashboardPaciente";
 
-// Funcionalidades
+
 import CrearMedico from "./pages/CrearMedico";
 import AgendarCita from "./pages/AgendarCita";
 import PerfilPaciente from "./pages/PerfilPaciente";
@@ -22,23 +22,23 @@ function App() {
     <Router>
       <Routes>
 
-        {/* LOGIN */}
+   
         <Route path="/" element={<Login />} />
 
-        {/* RECUPERAR */}
+    
         <Route path="/recover" element={<Recover />} />
 
-        {/* REGISTRO */}
+  
         <Route path="/register-paciente" element={<RegisterPaciente />} />
 
-        {/* ADMIN */}
+     
         <Route path="/dashboard-admin" element={<DashboardAdmin />} />
         <Route path="/crear-medico" element={<CrearMedico />} />
 
-        {/* MEDICO */}
+    
         <Route path="/dashboard-medico" element={<DashboardMedico />} />
 
-        {/* PACIENTE */}
+      
         <Route path="/dashboard-paciente" element={<DashboardPaciente />} />
         <Route path="/agendar-cita" element={<AgendarCita />} />
         <Route path="/perfil" element={<PerfilPaciente />} />
